bcdb9286b1598e294ba6f48f95906ee93f21da96
Elizabeth Dimbath <ed214@duke.edu>
2023-02-03 12:03:26 -0500

    addition of information in class, needs more updating

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 def create_patient_entry(patient_name, patient_mrn, patient_age):
-    new_patient = [patient_name, patient_mrn, patient_age]
+    new_patient = [patient_name, patient_mrn, patient_age ,[]]
     return new_patient
     
     
@@ -9,18 +9,43 @@ def main_driver():
     db.append(create_patient_entry("Bob Boyles", 2, 45))
     db.append(create_patient_entry("Chris Chou", 3, 52))
     print(db)
-    print("Get patient Ann")
-    found_patient = get_patient_entry(db, 4)
-    if found_patient is False:
-        print("patient mrn {} is not found".format(mrn_to_find))
-    else:
-        print(found_patient)
+    add_test_to_patient(db, 1, "HDL", 120)
+    add_test_to_patient(db, 2, "LDL", 100)
+    room_numbers = ["103", "232", "333"]
+    print(db)
+    print_directory(db, room_numbers)
+    print(
+
+def get_test_value_from_list(test_list, test_name):
+    for test in test_list
+        if test[0] == test_name:
+            return test[1]
+            
+def get_test_result(db, mrn, test_name):
+    patient = get_patient_entry(db, mrn)
+    test_value = get_test_value_from_list
+    return test_value
+
+def print_directory(db, room_numbers):
+    for i, patient in enumerate(db):
+        print("Patient {} is in room {}".format(patient[0], room_numbers[i]))
+    for patient, rn in zip(db, room_numbers):
+        print("Patient {} is in room {}".format(patient[0], rn))
     
 def get_patient_entry(db, mrn_to_find):
     for patient in db:
         if patient[1] == mrn_to_find:
             return patient
     return False 
+    
+def add_test_to_patient(db, mrn_to_find, test_name, test_value):
+    patient = get_patient_entry(db, mrn_to_find)
+    if patient == False:
+        print("Bad Entry")
+    else:
+        patient[3].append([test_name, test_value])
+    return
+    
     
     
 if __name__ == "__main__":
