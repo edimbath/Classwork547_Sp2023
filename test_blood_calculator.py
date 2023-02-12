a5ddb4c263ba8d27c5e73c49113c81002a4f18cd
Elizabeth Dimbath <ed214@duke.edu>
2023-02-12 11:09:52 -0500

    fixed PEP8 style in all previously developed files that did not use PEP8

--- test_blood_calculator.py
+++ test_blood_calculator.py
@@ -1,32 +1,32 @@
 import pytest
 
-@pytest.mark.parametrize("HDL_input, expected",
-[(65, "Normal"), 
- (45, "Borderline Low"), 
- (20, "Low")
- ])
 
+@pytest.mark.parametrize("HDL_input, expected",
+                         [(65, "Normal"),
+                          (45, "Borderline Low"),
+                          (20, "Low")
+                          ])
 def test_HDL_analysis(HDL_input, expected):
     from blood_calculator import HDL_analysis
-    #arrange
-    #inputs of function
-    #act
+    # arrange
+    # inputs of function
+    # act
     answer = HDL_analysis(HDL_input)
-    #assert
+    # assert
     assert answer == expected
-    
-@pytest.mark.parametrize("LDL_input, expected", 
-[(120, "Normal"), 
- (135, "Borderline High"), 
- (170, "High"),
- (200, "Very High")
- ])
 
+
+@pytest.mark.parametrize("LDL_input, expected",
+                         [(120, "Normal"),
+                          (135, "Borderline High"),
+                          (170, "High"),
+                          (200, "Very High")
+                          ])
 def test_LDL_analysis(LDL_input, expected):
     from blood_calculator import LDL_analysis
-    #arrange
-    #inputs of function
-    #act
+    # arrange
+    # inputs of function
+    # act
     answer = LDL_analysis(LDL_input)
-    #assert
+    # assert
     assert answer == expected
